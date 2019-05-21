a4d4eb660490e5286dfd17af73134cb063fd3e72
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-05-21 10:02:03 -0700

    Update template.sh

--- template.sh
+++ template.sh
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 
-BINPATH="$(perl -MCwd -le 'print Cwd::abs_path(shift)' "${BASH_SOURCE[0]}")"
 # cd into folder where the script is located, or quit (defensive)
 # Works even if the executable is a symlink, and on macOS and Linux.
-#cd "$(dirname "$BINPATH")" || exit 1
+# Uses perl because this incantation was tested to be faster than alternatives.
+# BINPATH="$(perl -MCwd -le 'print Cwd::abs_path(shift)' "${BASH_SOURCE[0]}")"
+# cd "$(dirname "$BINPATH")" || exit 1
 
 # ----- logging & colors -------------------------------------------------- {{{
 
@@ -15,10 +16,9 @@ yellow=$'\x1b[0;33m'
 cyan=$'\x1b[0;36m'
 cnone=$'\x1b[0m'
 
+USE_COLOR=
 if [ -t 1 ]; then
   USE_COLOR=1
-else
-  USE_COLOR=0
 fi
 
 # Detects whether we can add colors or not
@@ -26,33 +26,37 @@ in_color() {
   local color="$1"
   shift
 
-  if [ "$USE_COLOR" = "1" ]; then
-    echo "$color$*$cnone"
-  else
+  if [ -z "$USE_COLOR" ]; then
     echo "$*"
+  else
+    echo "$color$*$cnone"
   fi
 }
 
 success() { echo "$(in_color "$green" "[ OK ]") $*"; }
-error() {   echo "$(in_color "$red"   "[ERR!]") $*"; }
-info() {    echo "$(in_color "$cyan"  "[INFO]") $*"; }
+error() { echo "$(in_color "$red" "[ERR!]") $*"; }
+info() { echo "$(in_color "$cyan" "[ .. ]") $*"; }
+fatal() { error "$@"; exit 1; }
 # Color entire warning to get users' attention (because we won't stop).
-warn() { in_color "$yellow" "[WARN] $*"; }
+warn() { in_color "$yellow" "[ .. ] $*"; }
 
 # }}}
 
 # ----- helper functions ------------------------------------------------------
 
-print_help() {
+usage() {
   cat <<EOF
 
-foo: TODO(jez) Name and description
+foo: TODO Name and description
 
 Usage:
-  foo [options] TODO(jez)
+  foo [options] TODO incantations
+
+Arguments:
+  TODO Required (positional) arguments
 
 Options:
-  TODO(jez)
+  TODO optional flags
 
 EOF
 }
@@ -69,10 +73,6 @@ done
 
 # ----- main ------------------------------------------------------------------
 
-main() {
-  echo 'Hello, world!'
-}
-
-main "$@"
+echo 'Hello, world!'
 
 # vim:fdm=marker
